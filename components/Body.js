aa6af707147438c5bf3f0e184cdf97d2049ed661
shabdendhu <tshabdendhu@gmail.com>
2022-09-18 09:09:39 +0530

    fb intigration

--- components/Body.js
+++ components/Body.js
@@ -493,10 +493,10 @@ const Body = () => {
         <iframe
           src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FShreeBhavaniEFM&tabs=timeline&width=350&height=600&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
           style={{ border: "none", overflow: "hidden",width:'500px' }}
-          scrolling="no"
-          frameborder="0"
-          allowfullscreen="true"
-          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+          // scrolling="no"
+          // frameborder="0"
+          // allowfullscreen="true"
+          // allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
         ></iframe>
        
       </div>
